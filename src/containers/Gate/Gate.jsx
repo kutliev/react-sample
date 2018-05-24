c7761daa237ba7d2881a7c7793dcfc9d324a9e51
Bulat Kutliev <ghostbulat@gmail.com>
2018-05-24 12:09:25 +0500

    Gate cleanup

--- src/containers/Gate/Gate.jsx
+++ src/containers/Gate/Gate.jsx
@@ -3,24 +3,34 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { authenticate, unauthenticate } from '../../store/actions';
 
-const Gate = ({ logIn, logOut }) => (
+const Gate = ({ isAuthenticated, logIn, logOut }) => (
   <div>
     <h2>Gate</h2>
     <div>
-      <button onClick={logIn}>LOG IN</button>
-      <button onClick={logOut}>LOG OUT</button>
+      {!isAuthenticated && (
+        <button onClick={logIn}>LOG IN</button>
+      )}
+
+      {isAuthenticated && (
+        <button onClick={logOut}>LOG OUT</button>
+      )}
     </div>
   </div>
 );
 
 Gate.propTypes = {
+  isAuthenticated: PropTypes.bool.isRequired,
   logIn: PropTypes.func.isRequired,
   logOut: PropTypes.func.isRequired,
 };
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.user.isAuthenticated,
+});
 
 const mapDispatchToProps = dispatch => ({
   logIn: () => dispatch(authenticate()),
   logOut: () => dispatch(unauthenticate()),
 });
 
-export default connect(null, mapDispatchToProps)(Gate);
+export default connect(mapStateToProps, mapDispatchToProps)(Gate);
